d9d8d86082a5e795fa6cc48030f98a4eef286d76
ofir geller <ofirgeller22@gmail.com>
2018-02-11 17:20:01 +0200

    correct the EntityTypeName of the key mappings that are different from the EntityTypeName of the deleted keys for some reason

--- Gust/Keys/EntityKey.cs
+++ Gust/Keys/EntityKey.cs
@@ -9,7 +9,13 @@
             EntityTypeName = type.Name + ":#" + type.Namespace;
             KeyValue = key;
         }
+
+        /// <summary>
+        /// Needs to be in the form of "Form:#LZDataBase.Model"
+        /// example: "Form:#LZDataBase.Model" where "LZDataBase.Model" is the namespace and "Form" is the type (class name)
+        /// </summary>
         public string EntityTypeName;
+
         public object KeyValue;
     }
 }
--- Gust/Keys/KeyMapping.cs
+++ Gust/Keys/KeyMapping.cs
@@ -2,8 +2,14 @@
 {
     public class KeyMapping
     {
+        /// <summary>
+        /// Must be in the form of "namespace.type"
+        /// example: "LZDataBase.Model.Form" where "LZDataBase.Model" is the namespace and "Form" is the class name (the type)
+        /// </summary>
         public string EntityTypeName;
+
         public object TempValue;
+
         public object RealValue;
     }
 }
