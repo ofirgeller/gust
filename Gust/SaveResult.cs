1ba404117a3586f3e0f2257d6c27e05ce2c3acfb
ofir geller <ofirgeller22@gmail.com>
2018-02-12 20:26:23 +0200

    add virtual methods to interfere with saving entities make it easier to create entity info from outside the library

--- Gust/SaveResult.cs
+++ Gust/SaveResult.cs
@@ -8,9 +8,26 @@ namespace Gust
     /// </summary>
     public class SaveResult
     {
+        /// <summary>
+        /// All entities that were updated, created or deleted. 
+        /// </summary>
         public List<object> Entities;
+
+        /// <summary>
+        /// All changes that were made to any of the keys belonging to any of the entities.
+        /// Most commonly the mapping will be between the temporary id the client used and the id the
+        /// DB gave the entity when it was created
+        /// </summary>
         public List<KeyMapping> KeyMappings;
+
+        /// <summary>
+        /// The keys of all the entities that were deleted as a result of the save operation
+        /// </summary>
         public List<EntityKey> DeletedKeys;
+
+        /// <summary>
+        /// Any errors that were encountered while performing the save operation
+        /// </summary>
         public List<object> Errors;
     }
 }
